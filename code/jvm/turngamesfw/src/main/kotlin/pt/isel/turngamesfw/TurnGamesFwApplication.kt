32d4de194af9465d1d4c7e3e5ee288601bfee421
David Lopes <david.mlopes15@gmail.com>
2023-03-27 15:58:16 +0100

    Generated Spring Project
    With Spring Web dependencies.
    
    Co-Authored-By: NunoBartolomeu <72549215+NunoBartolomeu@users.noreply.github.com>

---
+++ code/jvm/turngamesfw/src/main/kotlin/pt/isel/turngamesfw/TurnGamesFwApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.turngamesfw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TurnGamesFwApplication
+
+fun main(args: Array<String>) {
+	runApplication<TurnGamesFwApplication>(*args)
+}
---
+++ code/jvm/turngamesfw/src/test/kotlin/pt/isel/turngamesfw/TurnGamesFwApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.turngamesfw
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TurnGamesFwApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
