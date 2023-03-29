d5eb3f468a163fdbd9ae8a502d317f8a926d967c
David Lopes <david.mlopes15@gmail.com>
2023-03-29 18:57:59 +0100

    Refactor DB schema
    Co-Authored-By: NunoBartolomeu <72549215+NunoBartolomeu@users.noreply.github.com>

--- code/jvm/turngamesfw/sql/create-schema.sql
+++ code/jvm/turngamesfw/sql/create-schema.sql
@@ -4,7 +4,13 @@ create table dbo.Users(
     id int generated always as identity primary key,
     username unique VARCHAR(64) not null,
     password_validation VARCHAR(256) not null,
-    state VARCHAR(64) not null CHECK (state in ('OFFLINE', 'ONLINE', 'SEARCHING', 'IN_GAME')) default 'OFFLINE',
+    state VARCHAR(64) not null CHECK (state in ('OFFLINE', 'ONLINE')) default 'OFFLINE'
+);
+
+create table dbo.Users_Stats(
+    user_id int primary key references dbo.Users(id),
+    game_id int primary key references dbo.Games(id),
+    state VARCHAR(64) CHECK (state in ('SEARCHING', 'IN_GAME')) default null,
     rating int not null
 );
 
@@ -16,13 +22,24 @@ create table dbo.Tokens(
 );
 
 create table dbo.Games(
+    id int generated always as identity primary key,
+    name VARCHAR(64) not null,
+    description VARCHAR(255),
+    rules VARCHAR(255)
+);
+
+create table dbo.Matches(
     id UUID primary key not null,
+    game_id int references dbo.Games(id),
     state VARCHAR(64) not null CHECK (state in ('SETUP', 'ON_GOING', 'END')) default 'SETUP',
+    curr_player int references dbo.Users(id),
+    curr_turn int not null default 0,
+    deadline_turn TIMESTAMP,
     info jsonb not null
 );
 
 create table dbo.GamesUsers(
-    gameId: UUID primary key references dbo.Games(id),
-    userId: int primary key references dbo.Users(id)
+    game_id UUID primary key references dbo.Games(id),
+    user_id int primary key references dbo.Users(id)
 );
 
