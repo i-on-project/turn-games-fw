388f47fec73dd9c381eb962c1e91d468a071882f
David Lopes <david.mlopes15@gmail.com>
2023-05-19 00:55:44 +0100

    Refactor deserializer from JsonNode

--- code/jvm/turngamesfw/tictactow-module/src/main/kotlin/pt/isel/tictactow/TicTacTowGameLogic.kt
+++ code/jvm/turngamesfw/tictactow-module/src/main/kotlin/pt/isel/tictactow/TicTacTowGameLogic.kt
@@ -1,12 +1,47 @@
 package pt.isel.tictactow
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
 import pt.isel.fwinterfaces.Game
 import pt.isel.fwinterfaces.GameLogic
 import pt.isel.fwinterfaces.Match
 import java.lang.IllegalArgumentException
 
 class TicTacTowGameLogic: GameLogic {
+
+    companion object {
+        class PositionDeserializer : JsonDeserializer<Position>() {
+            override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): Position {
+                val node: JsonNode = parser.codec.readTree(parser)
+                val col: Int = node.get("col").asInt()
+                val row: Int = node.get("row").asInt()
+                return Position(col, row)
+            }
+        }
+
+        class BoardDeserializer : JsonDeserializer<Board>() {
+            override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): Board {
+                val node: JsonNode = parser.codec.readTree(parser)
+                val cellsNode: JsonNode = node.get("cells")
+                val cells: Array<Array<Board.State>> = cellsNode.map { rowNode ->
+                    rowNode.map { cellNode ->
+                        val stateName: String = cellNode.asText()
+                        val state: Board.State = Board.State.valueOf(stateName)
+                        state
+                    }.toTypedArray()
+                }.toTypedArray()
+                return Board(cells)
+            }
+        }
+
+        private val modulePosition: SimpleModule = SimpleModule().addDeserializer(Position::class.java, PositionDeserializer())
+        private val moduleBoard: SimpleModule = SimpleModule().addDeserializer(Board::class.java, BoardDeserializer())
+        private val objectMapper: ObjectMapper = ObjectMapper().registerModule(modulePosition).registerModule(moduleBoard)
+    }
 
     override fun create(users: List<Int>): Match {
         if (users.size != 2) {
@@ -19,7 +54,7 @@ class TicTacTowGameLogic: GameLogic {
             players = users,
             currPlayer = users.first(),
             currTurn = 1,
-            info = ObjectMapper().valueToTree(Board.create())
+            info = objectMapper.valueToTree(Board.create())
         )
     }
 
@@ -28,7 +63,8 @@ class TicTacTowGameLogic: GameLogic {
     }
 
     override fun doTurn(match: Match, infoTurn: GameLogic.InfoTurn): GameLogic.UpdateInfo {
-        return applyRound(match, infoTurn.playerId, ObjectMapper().treeToValue(infoTurn.info, Position::class.java))
+        val position = objectMapper.treeToValue(infoTurn.info, Position::class.java)
+        return applyRound(match, infoTurn.playerId, position)
     }
 
     override fun matchPlayerView(match: Match, playerId: Int): Match {
@@ -65,20 +101,20 @@ class TicTacTowGameLogic: GameLogic {
                     playerNext = 0
                     Board.State.PLAYER_O
                 }
-                val board: Board = ObjectMapper().treeToValue(match.info, Board::class.java)
+                val board: Board = objectMapper.treeToValue(match.info, Board::class.java)
                 if (!board.canPlayOn(position)) {
                     return GameLogic.UpdateInfo(true, "Position not available!", null)
                 }
 
                 val newBoard = board.mutate(position, playerState)
                 if (newBoard.hasWon(playerState)) {
-                    val newMatch = match.copy(info = ObjectMapper().valueToTree(newBoard), state = Match.State.FINISHED)
+                    val newMatch = match.copy(info = objectMapper.valueToTree(newBoard), state = Match.State.FINISHED)
                     return GameLogic.UpdateInfo(false, "Player ${playerState.char} won! Game Ended.", newMatch)
                 }
 
                 if (newBoard.isFull()) {
                     val newMatch = match.copy(
-                        info = ObjectMapper().valueToTree(newBoard),
+                        info = objectMapper.valueToTree(newBoard),
                         state = Match.State.FINISHED,
                     )
                     return GameLogic.UpdateInfo(
@@ -88,7 +124,7 @@ class TicTacTowGameLogic: GameLogic {
                     )
                 } else {
                     val newMatch = match.copy(
-                        info = ObjectMapper().valueToTree(newBoard),
+                        info = objectMapper.valueToTree(newBoard),
                         currPlayer = match.players[playerNext],
                         currTurn = match.currTurn + 1,
                     )
